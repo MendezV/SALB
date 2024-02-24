9b345f8214a9b479cd686e1531a27e95ee10e5c3
MendezV <som.felipe66@gmail.com>
2024-02-24 17:47:53 -0500

    prep_dir_working

--- scripts/prep_dir.py
+++ scripts/prep_dir.py
@@ -12,25 +12,40 @@ EXEC_FILE = os.path.join(SALB_DIR,"build/SSE")  # Replace with your template fil
 
 
 # Function to modify the template file
-def modify_template(file_path, parameter_name, parameter_value):
-    PARAMETER_NAME = parameter_name +"="
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+def modify_template(file_path, sim):
+    
+    # looping over the dictionary that contains the parameters of the simulation
+    for parameter_name in sim:
+        
+        parameter_value = sim[parameter_name] #parameter value 
+        PARAMETER_NAME = parameter_name +"=" #formatting 
+        
+        #reading the file
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
 
-    with open(file_path, 'w') as file:
-        for line in lines:
-            if line.startswith(PARAMETER_NAME):
-                file.write(f"{PARAMETER_NAME}{parameter_value}\n")
-            else:
-                file.write(line)
+        #writting a new file but modifying the line that contains the parameter
+        with open(file_path, 'w') as file:
+            for line in lines:
+                if line.startswith(PARAMETER_NAME):
+                    file.write(f"{PARAMETER_NAME}{parameter_value}\n")
+                else:
+                    file.write(line)
 
-PARAMETERS = [1.0, 2.0, 3.0]  # List of parameter values
-param1 = "beta"
+def gen_sim_name(jobname, sim):
+    
+    name = jobname
+    for parameter_name in sim:
+        name= f"{name}_{parameter_name}_{sim[parameter_name]}"
+        
+    return name
 
-# Main script
-for param in PARAMETERS:
+
+def prep_sim_dir(sim):
+    
     # Create subdirectory for each parameter
-    sub_dir = os.path.join(TEMP_DIR, f"heisenberg_{param1}_{param}")
+    simname  = gen_sim_name(jobname, sim)
+    sub_dir = os.path.join(TEMP_DIR, simname)
     os.makedirs(sub_dir, exist_ok=True)
 
     # Copy the template file to the subdirectory
@@ -38,10 +53,38 @@ for param in PARAMETERS:
     shutil.copy(TEMPLATE_FILE, sub_file_path)
 
     # Modify the template file in the subdirectory
-    modify_template(sub_file_path, param1,  param)
+    modify_template(sub_file_path, sim)
     
     # Copy the executable file to the subdirectory
     sub_file_path = os.path.join(sub_dir, "SSE")
     shutil.copy(EXEC_FILE, sub_file_path)
 
+    return None
+    
+
+jobname = "heisenberg"
+sims = []
+betavals = [1.0,2.0,3.0]
+Lvals = [8,12,16]
+
+for beta in betavals:
+    for L in Lvals:
+        
+        sim = {
+            "beta":beta,
+            "Lx":L,
+            "Ly":L,
+            "Lz":L
+        }
+        
+        sims.append(sim)
+        
+
+
+# Main script
+for sim in sims:
+   
+   prep_sim_dir(sim) 
+   
+   
 print("Script executed. Directories created and files modified according to the parameters.")
